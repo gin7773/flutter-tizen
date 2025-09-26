719db0b92d80cf482815c39daa03069f5acfbaaa
yying.jin <yying.jin@samsung.com>
2025-09-26 10:21:07 +0800

    test

--- lib/build_targets/application.dart
+++ lib/build_targets/application.dart
@@ -14,6 +14,7 @@ import 'package:flutter_tools/src/build_system/targets/android.dart';
 import 'package:flutter_tools/src/build_system/targets/assets.dart';
 import 'package:flutter_tools/src/build_system/targets/common.dart';
 import 'package:flutter_tools/src/build_system/targets/icon_tree_shaker.dart';
+import 'package:flutter_tools/src/build_system/targets/native_assets.dart';
 import 'package:flutter_tools/src/compile.dart';
 import 'package:flutter_tools/src/dart/package_map.dart';
 import 'package:package_config/src/package_config.dart';
@@ -40,18 +41,28 @@ class TizenKernelSnapshotProgram extends KernelSnapshot {
     }
     final BuildMode buildMode = BuildMode.fromCliName(buildModeEnvironment);
     final String targetFile =
-        environment.defines[kTargetFile] ?? environment.fileSystem.path.join('lib', 'main.dart');
-    final File packagesFile =
-        environment.projectDir.childDirectory('.dart_tool').childFile('package_config.json');
-    final String targetFileAbsolute = environment.fileSystem.file(targetFile).absolute.path;
+        environment.defines[kTargetFile] ??
+        environment.fileSystem.path.join('lib', 'main.dart');
+    final File packagesFile = environment.projectDir
+        .childDirectory('.dart_tool')
+        .childFile('package_config.json');
+    final String targetFileAbsolute = environment.fileSystem
+        .file(targetFile)
+        .absolute
+        .path;
     // everything besides 'false' is considered to be enabled.
-    final bool trackWidgetCreation = environment.defines[kTrackWidgetCreation] != 'false';
+    final bool trackWidgetCreation =
+        environment.defines[kTrackWidgetCreation] != 'false';
 
     // This configuration is all optional.
-    final String? frontendServerStarterPath = environment.defines[kFrontendServerStarterPath];
-    final List<String> extraFrontEndOptions =
-        decodeCommaSeparated(environment.defines, kExtraFrontEndOptions);
-    final List<String>? fileSystemRoots = environment.defines[kFileSystemRoots]?.split(',');
+    final String? frontendServerStarterPath =
+        environment.defines[kFrontendServerStarterPath];
+    final List<String> extraFrontEndOptions = decodeCommaSeparated(
+      environment.defines,
+      kExtraFrontEndOptions,
+    );
+    final List<String>? fileSystemRoots = environment.defines[kFileSystemRoots]
+        ?.split(',');
     final String? fileSystemScheme = environment.defines[kFileSystemScheme];
 
     final PackageConfig packageConfig = await loadPackageConfigWithLogging(
@@ -59,7 +70,9 @@ class TizenKernelSnapshotProgram extends KernelSnapshot {
       logger: environment.logger,
     );
 
-    final String dillPath = environment.buildDir.childFile(KernelSnapshot.dillName).path;
+    final String dillPath = environment.buildDir
+        .childFile(KernelSnapshot.dillName)
+        .path;
 
     final CompilerOutput? output = await compiler.compile(
       sdkRoot: environment.artifacts.getArtifactPath(
@@ -68,7 +81,8 @@ class TizenKernelSnapshotProgram extends KernelSnapshot {
       ),
       aot: buildMode.isPrecompiled,
       buildMode: buildMode,
-      trackWidgetCreation: trackWidgetCreation && buildMode != BuildMode.release,
+      trackWidgetCreation:
+          trackWidgetCreation && buildMode != BuildMode.release,
       outputFilePath: dillPath,
       initializeFromDill: buildMode.isPrecompiled ? null : dillPath,
       packagesPath: packagesFile.path,
@@ -95,9 +109,7 @@ class TizenKernelSnapshot extends KernelSnapshot {
   const TizenKernelSnapshot();
 
   @override
-  List<Target> get dependencies => const <Target>[
-        TizenKernelSnapshotProgram(),
-      ];
+  List<Target> get dependencies => const <Target>[TizenKernelSnapshotProgram()];
 }
 
 /// Prepares the pre-built Flutter bundle.
@@ -111,22 +123,21 @@ abstract class TizenAssetBundle extends Target {
 
   @override
   List<Source> get inputs => const <Source>[
-        Source.pattern('{BUILD_DIR}/app.dill'),
-        ...IconTreeShaker.inputs,
-      ];
+    Source.pattern('{BUILD_DIR}/app.dill'),
+    ...IconTreeShaker.inputs,
+  ];
 
   @override
   List<Source> get outputs => const <Source>[];
 
   @override
-  List<String> get depfiles => <String>[
-        'flutter_assets.d',
-      ];
+  List<String> get depfiles => <String>['flutter_assets.d'];
 
   @override
   List<Target> get dependencies => const <Target>[
-        TizenKernelSnapshot(),
-      ];
+    TizenKernelSnapshot(),
+    InstallCodeAssets(),
+  ];
 
   @override
   Future<void> build(Environment environment) async {
@@ -135,15 +146,20 @@ abstract class TizenAssetBundle extends Target {
       throw MissingDefineException(kBuildMode, name);
     }
     final BuildMode buildMode = BuildMode.fromCliName(buildModeEnvironment);
-    final Directory outputDirectory = environment.buildDir.childDirectory('flutter_assets')
-      ..createSync(recursive: true);
+    final Directory outputDirectory = environment.buildDir.childDirectory(
+      'flutter_assets',
+    )..createSync(recursive: true);
 
     // Only copy the prebuilt runtimes and kernel blob in debug mode.
     if (buildMode == BuildMode.debug) {
-      final String vmSnapshotData =
-          environment.artifacts.getArtifactPath(Artifact.vmSnapshotData, mode: BuildMode.debug);
-      final String isolateSnapshotData = environment.artifacts
-          .getArtifactPath(Artifact.isolateSnapshotData, mode: BuildMode.debug);
+      final String vmSnapshotData = environment.artifacts.getArtifactPath(
+        Artifact.vmSnapshotData,
+        mode: BuildMode.debug,
+      );
+      final String isolateSnapshotData = environment.artifacts.getArtifactPath(
+        Artifact.isolateSnapshotData,
+        mode: BuildMode.debug,
+      );
       environment.buildDir
           .childFile('app.dill')
           .copySync(outputDirectory.childFile('kernel_blob.bin').path);
@@ -186,21 +202,23 @@ class TizenAotElf extends AotElfBase {
 
   @override
   List<Source> get inputs => <Source>[
-        const Source.pattern('{BUILD_DIR}/app.dill'),
-        const Source.artifact(Artifact.engineDartBinary),
-        const Source.artifact(Artifact.skyEnginePath),
-        Source.artifact(Artifact.genSnapshot, platform: targetPlatform, mode: buildMode),
-      ];
+    const Source.pattern('{BUILD_DIR}/app.dill'),
+    const Source.artifact(Artifact.engineDartBinary),
+    const Source.artifact(Artifact.skyEnginePath),
+    Source.artifact(
+      Artifact.genSnapshot,
+      platform: targetPlatform,
+      mode: buildMode,
+    ),
+  ];
 
   @override
   List<Source> get outputs => const <Source>[
-        Source.pattern('{BUILD_DIR}/app.so'),
-      ];
+    Source.pattern('{BUILD_DIR}/app.so'),
+  ];
 
   @override
-  List<Target> get dependencies => const <Target>[
-        TizenKernelSnapshot(),
-      ];
+  List<Target> get dependencies => const <Target>[TizenKernelSnapshot()];
 }
 
 /// Source: [DebugAndroidApplication] in `android.dart`
@@ -214,24 +232,24 @@ class DebugTizenApplication extends TizenAssetBundle {
 
   @override
   List<Source> get inputs => <Source>[
-        ...super.inputs,
-        const Source.artifact(Artifact.vmSnapshotData, mode: BuildMode.debug),
-        const Source.artifact(Artifact.isolateSnapshotData, mode: BuildMode.debug),
-      ];
+    ...super.inputs,
+    const Source.artifact(Artifact.vmSnapshotData, mode: BuildMode.debug),
+    const Source.artifact(Artifact.isolateSnapshotData, mode: BuildMode.debug),
+  ];
 
   @override
   List<Source> get outputs => <Source>[
-        ...super.outputs,
-        const Source.pattern('{BUILD_DIR}/flutter_assets/vm_snapshot_data'),
-        const Source.pattern('{BUILD_DIR}/flutter_assets/isolate_snapshot_data'),
-        const Source.pattern('{BUILD_DIR}/flutter_assets/kernel_blob.bin'),
-      ];
+    ...super.outputs,
+    const Source.pattern('{BUILD_DIR}/flutter_assets/vm_snapshot_data'),
+    const Source.pattern('{BUILD_DIR}/flutter_assets/isolate_snapshot_data'),
+    const Source.pattern('{BUILD_DIR}/flutter_assets/kernel_blob.bin'),
+  ];
 
   @override
   List<Target> get dependencies => <Target>[
-        ...super.dependencies,
-        NativePlugins(buildInfo),
-      ];
+    ...super.dependencies,
+    NativePlugins(buildInfo),
+  ];
 }
 
 /// See: [ReleaseAndroidApplication] in `android.dart`
@@ -245,8 +263,11 @@ class ReleaseTizenApplication extends TizenAssetBundle {
 
   @override
   List<Target> get dependencies => <Target>[
-        ...super.dependencies,
-        TizenAotElf(getTargetPlatformForArch(buildInfo.targetArch), buildInfo.buildInfo.mode),
-        NativePlugins(buildInfo),
-      ];
+    ...super.dependencies,
+    TizenAotElf(
+      getTargetPlatformForArch(buildInfo.targetArch),
+      buildInfo.buildInfo.mode,
+    ),
+    NativePlugins(buildInfo),
+  ];
 }
